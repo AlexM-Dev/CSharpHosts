365d8ac6f32a13522b988bdf44a7cd6e87b73561
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-04-08 14:52:58 +1000

    Add project files.

---
+++ CSharpHosts/HostEntry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpHosts
+{
+    public class HostEntry {
+        public bool Enabled { get; set; }
+        public string IpAddress { get; set; }
+        public string WebAddress { get; set; }
+        public string Comment { get; set; }
+
+        public override string ToString() {
+            // Basic string concatenation, in host file entry form.
+            return
+                (!Enabled ? "#DISABLED " : "")
+                + IpAddress + " " + WebAddress + " " +
+                (Comment == "" ? "" : ("#" + Comment));
+        }
+        public HostEntry() { }
+
+        public HostEntry(bool enabled,
+            string ipAddress,
+            string webAddress,
+            string comment) {
+
+            this.Enabled = enabled;
+            this.IpAddress = ipAddress;
+            this.WebAddress = webAddress;
+            this.Comment = comment;
+        }
+        public static HostEntry FromLine(string line) {
+            // If the string begins with "#DISABLED " then it should
+            // return disabled = !enabled.
+            // Alternative: 
+            //      -> Regex.Matches(line, "^#DISABLED (.*?)").Count < 1;
+            bool enabled = !line.StartsWith("#DISABLED ");
+            // Any string before a #, a # indicates a comment. If the
+            // count is more or equal to one, it has a comment.
+            bool commented = Regex.Matches(line, ".+#(.*)?$").Count >= 1;
+
+            // If it's disabled, account for the that in the regex.
+            // else, have a standard regex matcher.
+            string regex = !enabled ? "^#DISABLED (.*?) (.*)" : "^(.*?) (.*)";
+            // If it contains a comment, then add regex to account
+            // for that comment. (#(.*)) matches everything beyond the
+            // # character.
+            regex += commented ? " #(.*)" : "";
+
+            // Get the only match in the string (assume `line` is one
+            // line only).
+            var g = Regex.Matches(line, regex)[0];
+
+            // g.Groups[1] = the ip,
+            // g.Groups[2] = the address,
+            // g.Groups[3] = the comment, if there is one.
+            HostEntry e = new HostEntry(enabled, g.Groups[1].Value,
+                g.Groups[2].Value, commented ? g.Groups[3].Value : "");
+            return e;
+        }
+    }
+}
---
+++ CSharpHosts/HostReader.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHosts {
+    class HostsReader {
+        // The default windows location for the hosts file.
+        private const string sysHosts =
+            @"C:\windows\system32\drivers\etc\Hosts";
+        public string FilePath { get; set; }
+        public List<string> Structure { get; set; }
+        public HostsReader() {
+            FilePath = sysHosts;
+        }
+
+        public HostsReader(string filePath) {
+            FilePath = filePath;
+        }
+
+        public bool Load() {
+            // Load the file to a list, for modification.
+            List<string> structure = new List<string>(
+                File.ReadAllLines(FilePath));
+
+            // Initialise the Structure list.
+            Structure = new List<string>();
+
+            foreach (var e in structure) {
+                // If e is a useless line, then add it as a useless line,
+                // else we should ensure the line is a perfect entry,
+                // and then add it to the Structure.
+                string entry = isUseless(e.Trim()) ? e.Trim() :
+                    HostEntry.FromLine(e).ToString().Trim();
+
+                Structure.Add(entry);
+            }
+
+            return true;
+        }
+
+        public bool Save() {
+            try {
+                File.WriteAllLines(FilePath, Structure);
+            } catch { return false; }
+
+            return true;
+        }
+
+        // int -> Reference to position in Structure.
+        // HostEntry -> Entry in file.
+        public Dictionary<int, HostEntry> GetEntries() {
+            var dict = new Dictionary<int, HostEntry>();
+
+            // Loop through each entry in the structure, determining if
+            // each item is a HostEntry or not.
+            for (int i = 0; i < Structure.Count; i++) {
+                string line = Structure[i];
+                // If not a useless line, add it to the result.
+                if (!isUseless(line))
+                    dict.Add(i, HostEntry.FromLine(line));
+            }
+
+            return dict;
+        }
+
+        public void Add(HostEntry entry) {
+            // Add the entry to the Structure.
+            Structure.Add(entry.ToString().Trim());
+        }
+        public void Remove(HostEntry entry) {
+            // Remove the entry to the Structure. Start from the end,
+            // so that any removals won't affect the loop + iteration.
+            for (int i = Structure.Count - 1; i > -1; i--) {
+                string decl = entry.ToString().Trim();
+                if (Structure[i].Trim() == decl)
+                    Structure.RemoveAt(i);
+            }
+        }
+
+        public KeyValuePair<int, HostEntry> this[int index] {
+            get {
+                // Get the first result of keys that match in
+                // GetEntries(), with the specified index.
+                return
+                    GetEntries().SingleOrDefault(p => p.Key == index);
+            }
+            set {
+                // Set the appropriate value in the Structure list,
+                // with the key specified in the Dictionary key.
+                Structure[value.Key] = value.Value.ToString().Trim();
+            }
+        }
+
+        /*
+         * Static methods
+         */
+
+        private static bool isUseless(string line) {
+            // If it's empty, then it's useless and should be
+            // disregarded as an entry.
+            if (string.IsNullOrWhiteSpace(line)) return true;
+            // If it doesn't start with a # (comment denotation)
+            // then it should be considered an entry. Not accounting
+            // for errors.
+            if (!line.StartsWith("#")) return false;
+            // If it starts with DISABLED, and there are >= 3 parts,
+            // then let's assume it's a disabled entry.
+            if (line.StartsWith("#DISABLED") &&
+                line.Split(' ').Count() >= 3) return false;
+
+            // Everything else, we assume is useless.
+            return true;
+        }
+    }
+}
